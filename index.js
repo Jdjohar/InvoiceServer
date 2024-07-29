32508e2bea0bfeb3ff3158da6df34fe99403c7f8
ForageIt <katariaesha02@gmail.com>
2024-07-29 17:57:15 +0530

    29-07-2024

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 ];
 if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin, X-Requested-With, Accept");
 }
   // res.setHeader("Access-Control-Allow-Origin", "* , https://restro-wbno.vercel.app");
